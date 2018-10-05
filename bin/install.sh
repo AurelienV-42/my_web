86fb2d2411ffbfa14d248f9da413775ae650cef1
Aurélien Vandaële <avandaele@acs-ami.com>
2018-10-05 16:48:32 +0200

    [1.0.3] Add install_epitech in the bin directory

--- bin/install.sh
+++ bin/install.sh
@@ -6,7 +6,7 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
 APP_DIR=$(cd ${SCRIPT_PATH}/.. && pwd)
-DIRECTORY_BLIH="./.files/"
+DIRECTORY_BLIH="${APP_DIR}/.files/"
 regex="^[Oo]([Uu][Ii])?$"
 
 echo "Début du script..."
@@ -20,12 +20,12 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Installation de zsh, curl et ssh
-sudo apt-get install zsh curl ssh htop tree terminator libncurses5 ocaml valgrind build-essential gcc intel-microcodel
+sudo apt-get install zsh curl ssh htop tree terminator libncurses5 ocaml valgrind build-essential gcc intel-microcode
 
 # Installation de oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "alias ne=\"emacs-nw\"" >> $HOME/.zshrc
+echo "alias ne=\"emacs -nw\"" >> $HOME/.zshrc
 echo "alias ga=\"git add --all\"" >> $HOME/.zshrc
 echo "alias gc=\"git commit -m\"" >> $HOME/.zshrc
 echo "alias gp=\"git push\"" >> $HOME/.zshrc
@@ -38,12 +38,17 @@ if [ ! -d $HOME/.bin ] ; then
     fi
 fi
 
-while [ add_alias == 0 ]; do
+echo "Installation de blih..."
+sudo cp "${DIRECTORY_BLIH}blih" /usr/bin/
+sudo chmod 755 /usr/bin/blih
+
+add_alias=0
+while [ $add_alias == 0 ]; do
     read -p "Souhaitez-vous créer des alias? (o/N): " answer
     if [[ "$answer" =~ $regex ]]; then
         ./create_alias
     else
-        add_alias = 1
+        add_alias=1
     fi
 done
 
@@ -52,12 +57,19 @@ if [[ "$answer" =~ $regex ]]; then
     ssh-keygen
 fi
 
-echo "mot de passe UNIX (bocal, pour blih)"
-blih -u "$1" sshkey upload $HOME/.ssh/id_rsa.pub
+read -p "Souhaitez-vous générer upload votre clé ssh ? (o/N): " answer
+if [[ "$answer" =~ $regex ]]; then
+    echo "Mot de passe UNIX (bocal, pour blih)"
+    blih -u "$1" sshkey upload $HOME/.ssh/id_rsa.pub
+fi
 
-# Changement des droits sur poweroff et reboot pour pouvoir poweroff et reboot sans sudo
+# Changement des droits sur poweroff et reboot pour pouvoir les utiliser sans sudo
 sudo chmod +s /sbin/poweroff
 sudo chmod +s /sbin/reboot
 
 # Reset sudo, next time you will need a password
 sudo -k
+
+echo "Script de configuration terminé."
+
+zsh
